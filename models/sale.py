91a2784eba88a53abdc8b01bc64c67ce1304de39
jeffrepo <jeffer.leo93@gmail.com>
2022-10-12 12:39:20 -0600

    Limite de descuentos SO

--- models/sale.py
+++ models/sale.py
@@ -14,9 +14,17 @@ class SaleOrder(models.Model):
                 autorizacion_descuento = []
                 if orden.order_line:
                     for linea in orden.order_line:
-                        if linea.discount > linea.product_id.descuento_maximo:
-                            autorizacion_descuento.append(' Nombre producto: ' + str(linea.product_id.name) + ' ,límite máximo: '+str(linea.product_id.descuento_maximo) + "% ")
+                        porcetaje_linea = self.porcetaje_linea(linea)
+                        if porcetaje_linea > 0 and  porcetaje_linea > linea.product_id.descuento_maximo:
+                            autorizacion_descuento.append(' Nombre producto: ' + str(linea.product_id.name) + ',límite máximo: '+str(linea.product_id.descuento_maximo) + "% ,pocentaje calculado: " + str(porcetaje_linea) + "%")
                 if len(autorizacion_descuento) == 0:
                     return super(SaleOrder, self).action_confirm()
                 else:
                     raise ValidationError(_('No tiene permiso para validar la orden por sobrepasar limite de descuentos' + str(autorizacion_descuento)))
+
+    def porcetaje_linea(self, linea):
+        porcentaje_descuento = 0
+        if linea.price_unit < linea.product_id.lst_price:
+            porcentaje = (linea.price_unit / linea.product_id.lst_price) * 100
+            porcentaje_descuento = round(100 - porcentaje, 2)
+        return float(porcentaje_descuento)
